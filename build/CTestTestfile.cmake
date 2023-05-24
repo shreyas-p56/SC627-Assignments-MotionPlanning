ee4fc1bec9f633c61ae420ad7b31a98ad9590280
Shreyas Patil <shreyasp958@gmail.com>
2023-05-24 16:17:45 +0530

    Create catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/xavier/SC627-Assignments-MotionPlanning/src
+# Build directory: /home/xavier/SC627-Assignments-MotionPlanning/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/xavier/SC627-Assignments-MotionPlanning/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/xavier/SC627-Assignments-MotionPlanning/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/xavier/SC627-Assignments-MotionPlanning/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/xavier/SC627-Assignments-MotionPlanning/src:$ROS_PACKAGE_PATH"
